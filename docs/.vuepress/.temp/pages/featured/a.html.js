3da4cb5320a248d82b59570dd5933084e1361ec5
lrsoy <lying_jk_bp@163.com>
2022-08-24 00:13:39 +0800

    重新构建

--- docs/.vuepress/.temp/pages/featured/a.html.js
+++ docs/.vuepress/.temp/pages/featured/a.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-360f0400\",\"path\":\"/featured/a.html\",\"title\":\"测试文件\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"featured/a.md\"}")
+export const data = JSON.parse("{\"key\":\"v-360f0400\",\"path\":\"/featured/a.html\",\"title\":\"测试文件\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[],\"git\":{\"updatedTime\":1661270471000,\"contributors\":[{\"name\":\"lrsoy\",\"email\":\"lying_jk_bp@163.com\",\"commits\":1}]},\"filePathRelative\":\"featured/a.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
